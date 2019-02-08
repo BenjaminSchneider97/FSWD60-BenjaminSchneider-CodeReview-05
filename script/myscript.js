57c6e62d20c70651deb63857e1f6e9fb396df18b
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-02-08 12:40:10 +0100

    adjust some pics again and add all movies

--- script/myscript.js
+++ script/myscript.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
-
-	for(i=0; i< comedy.length; i++){
-		$("#allmovies").append(`
+$(document).ready(function() {
+    for (i = 0; i < comedy.length; i++) {
+        $("#allmovies").append(`
 			<div class='col-lg-5 movie comedy'>
 				<div class=" col-lg-5 movieimg">
 					<img src="${comedy[i].image}">
@@ -12,5 +11,44 @@ $(document).ready(function(){
 					<p>${comedy[i].desc}</p>
 				</div>
 			</div>`);
-	}
+    }
+    for (i = 0; i < action.length; i++) {
+        $("#allmovies").append(`
+			<div class='col-lg-5 movie action'>
+				<div class=" col-lg-5 movieimg">
+					<img src="${action[i].image}">
+				</div>
+				<div class="col-lg-7 description">
+					<h3>${action[i].title}</h3>
+					<hr>
+					<p>${action[i].desc}</p>
+				</div>
+			</div>`);
+    }
+    for (i = 0; i < drama.length; i++) {
+        $("#allmovies").append(`
+			<div class='col-lg-5 movie action'>
+				<div class=" col-lg-5 movieimg">
+					<img src="${drama[i].image}">
+				</div>
+				<div class="col-lg-7 description">
+					<h3>${drama[i].title}</h3>
+					<hr>
+					<p>${drama[i].desc}</p>
+				</div>
+			</div>`);
+    }
+    for (i = 0; i < horror.length; i++) {
+        $("#allmovies").append(`
+			<div class='col-lg-5 movie action'>
+				<div class=" col-lg-5 movieimg">
+					<img src="${horror[i].image}">
+				</div>
+				<div class="col-lg-7 description">
+					<h3>${horror[i].title}</h3>
+					<hr>
+					<p>${horror[i].desc}</p>
+				</div>
+			</div>`);
+    }
 });
